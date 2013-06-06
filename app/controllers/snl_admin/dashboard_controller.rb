8b627598b97ccdecbe822a11228686db3cabc088
Peter Kordel <pkordel@gmail.com>
2013-06-07 00:07:04 +0200

    Uncoupling from main app

--- app/controllers/snl_admin/dashboard_controller.rb
+++ app/controllers/snl_admin/dashboard_controller.rb
@@ -3,16 +3,6 @@ require_dependency "snl_admin/application_controller"
 module SnlAdmin
   class DashboardController < ApplicationController
     def index
-      @performers = User
-        .select([:id, :firstname, :lastname, :num_characters_changed, :role])
-        .order('num_characters_changed desc')
-        .limit(5)
-      @earners = User
-        .where("role = ? OR role = ? OR role = ?",
-          'authoritative_editor', 'sub_editor', 'bughunter')
-        .select([:id, :firstname, :lastname, :num_characters_changed, :role, :fixed_ceiling])
-        .order('num_characters_changed desc')
-        .limit(5)
     end
   end
 end
--- lib/snl_admin/engine.rb
+++ lib/snl_admin/engine.rb
@@ -1,5 +1,11 @@
 module SnlAdmin
   class Engine < ::Rails::Engine
     isolate_namespace SnlAdmin
+
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
