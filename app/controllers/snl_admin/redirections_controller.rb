fff5a4d54589a783508393f58e03c11937d757f9
Erling Wegger Linde <erlingwl@gmail.com>
2013-09-30 20:33:39 +0200

    create, edit and destroy redirections

--- app/controllers/snl_admin/redirections_controller.rb
+++ app/controllers/snl_admin/redirections_controller.rb
@@ -3,6 +3,7 @@ require_dependency "snl_admin/application_controller"
 
 module SnlAdmin
   class RedirectionsController < ApplicationController
+    before_filter :set_redirection
     respond_to :html
 
     def index
@@ -14,6 +15,56 @@ module SnlAdmin
       else
         Redirection.order('headword asc')
       end.page params[:page]
+    end
+
+    def new
+      @title = t('new_redirection')
+    end
+
+    def edit
+      @title = t('edit_user')
+    end
+
+    def show
+    end
+
+    def create
+      @redirection = SnlAdmin.redirection_class.new redirection_params
+      if @redirection.save
+        respond_with @redirection
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @redirection.update_attributes redirection_params
+        respond_with @redirection
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @redirection.destroy
+      notice = "Slettet redirectionen"
+      redirect_to redirections_path, notice: notice
+    end
+
+    private
+
+    def redirection_params
+      valid_params = params.require(:redirection).
+             permit(:urlpath, :headword, :clarification, :article_id)
+      valid_params[:urlpath] = nil if valid_params[:urlpath].empty?
+      valid_params[:clarification] = nil if valid_params[:clarification].empty?
+      valid_params
+    end
+
+    def set_redirection
+      @redirection = params[:id] ?
+        SnlAdmin.redirection_class.find(params[:id]) :
+        SnlAdmin.redirection_class.new()
     end
 
   end
