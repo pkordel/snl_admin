bb23b47015665d86d24d46a010c3502783255599
Erling Wegger Linde <erlingwl@gmail.com>
2014-01-21 14:38:32 +0100

    support escaping/encoding of redirections

--- lib/snl_admin.rb
+++ lib/snl_admin.rb
@@ -4,6 +4,7 @@ module SnlAdmin
   # This is where global configuration options can go
   mattr_accessor :user_class
   mattr_accessor :redirection_class
+  mattr_accessor :permalink_class
 
   def self.user_class
     @@user_class.constantize
@@ -11,5 +12,9 @@ module SnlAdmin
 
   def self.redirection_class
     @@redirection_class.constantize
+  end
+
+  def self.permalink_class
+    @@permalink_class.constantize
   end
 end
