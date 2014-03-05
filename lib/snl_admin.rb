d0c338ff08a0c3a56f6b62e3504472ff984698cb
Peter Kordel <pkordel@gmail.com>
2014-03-05 15:43:02 +0700

    Add class methods for license and taxonomy

--- lib/snl_admin.rb
+++ lib/snl_admin.rb
@@ -4,6 +4,8 @@ module SnlAdmin
   # This is where global configuration options can go
   mattr_accessor :user_class
   mattr_accessor :redirection_class
+  mattr_accessor :license_class
+  mattr_accessor :taxonomy_class
 
   def self.user_class
     @@user_class.constantize
@@ -11,5 +13,13 @@ module SnlAdmin
 
   def self.redirection_class
     @@redirection_class.constantize
+  end
+
+  def self.license_class
+    @@license_class.constantize
+  end
+
+  def self.taxonomy_class
+    @@taxonomy_class.constantize
   end
 end
