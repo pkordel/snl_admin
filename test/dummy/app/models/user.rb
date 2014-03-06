5249a6c6993d8a6a433282fa79e6ab50c5ed7dfb
Peter Kordel <pkordel@gmail.com>
2014-03-06 16:09:11 +0700

    Move to main app

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
+  include ActiveModel::ForbiddenAttributesProtection
 
   ROLES = %w(editor authoritative_editor sub_editor contributor bughunter)
 
+  EDITOR        = 'editor'
+  CONTRIBUTOR   = 'contributor'
+
   def public_name
     (self.firstname.to_s + ' ' + self.lastname).strip
-  end
-  
-  def confirmed?
-    !!confirmed_at
   end
 end
