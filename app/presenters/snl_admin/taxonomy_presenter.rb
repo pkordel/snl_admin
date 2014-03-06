929f88d958728de76f08fd6050511e1cd53496c4
Peter Kordel <pkordel@gmail.com>
2014-03-06 16:08:18 +0700

    Move this to main app also

--- app/presenters/snl_admin/taxonomy_presenter.rb
+++ app/presenters/snl_admin/taxonomy_presenter.rb
@@ -1,14 +1,27 @@
 module SnlAdmin
   class TaxonomyPresenter < SimpleDelegator
-    attr_reader :taxonomy
+    attr_reader :taxonomy, :context
 
-    def initialize(taxonomy)
+    def initialize(taxonomy, context)
       super(taxonomy)
       @taxonomy = taxonomy
+      @context  = context
     end
 
     def title
-      taxonomy.title
+      @title ||= taxonomy.title
+    end
+
+    def description
+      @description ||= taxonomy.description
+    end
+
+    def parents_path
+      taxonomy.ancestors.select([:id, :title])
+        .to_a
+        .inject([]) do |ary, parent|
+          ary << context.link_to(parent.title, parent)
+        end.join(' &raquo; ').html_safe
     end
   end
 end
