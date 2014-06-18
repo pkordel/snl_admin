5948c58280f84434dddce110ea07e535a8855f39
Erling Wegger Linde <erlingwl@gmail.com>
2014-06-18 12:54:19 +0200

    tweak nav_link

--- app/helpers/snl_admin/application_helper.rb
+++ app/helpers/snl_admin/application_helper.rb
@@ -7,9 +7,14 @@ module SnlAdmin
       title ||= t("#{resource}.title")
 
       name = options.delete(:name) || title
-      path = options.delete(:path) || send("#{resource}_path") rescue '#not-implemented'
-      css  = controller_name == resource ? 'active' : ''
-
+      path_options = options.delete(:path_options) || {}
+      path = options.delete(:path) || send("#{resource}_path", path_options) rescue '#not-implemented'
+      matches_controller_name = controller_name == resource
+      matches_path_options = true
+      path_options.each do |key, value|
+        matches_path_options = params[key] == value
+      end
+      css  = (matches_controller_name && matches_path_options) ? 'active' : ''
       content_tag(:li,  class: css) do
         link_to(name, path, options)
       end
