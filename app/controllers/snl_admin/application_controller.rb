eeb4f6ac953ed1c486d3c2d085663e1fb66a3bad
Peter Kordel <pkordel@gmail.com>
2016-05-31 14:50:05 +0700

    Adds a view_url override to avoid double encoding of permalinks

--- app/controllers/snl_admin/application_controller.rb
+++ app/controllers/snl_admin/application_controller.rb
@@ -8,5 +8,15 @@ module SnlAdmin
       @current_user ||= SnlAdmin.user_class.find_by_id(session[:user_id])
     end
     helper_method :current_user
+
+    def view_url(permalink, subdomain: nil, anchor: nil)
+      host      = request.domain || request.host
+      fullpath  = request.protocol.to_s
+      fullpath += "#{subdomain}." unless subdomain.blank?
+      fullpath += "#{host}#{request.port_string}/#{permalink}"
+      fullpath += "##{anchor}" unless anchor.blank?
+      fullpath
+    end
+    helper_method :view_url
   end
 end
