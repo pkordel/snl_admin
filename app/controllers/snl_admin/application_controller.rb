7933961909959b02e1ea48c52d73c331464d0249
Peter Kordel <pkordel@gmail.com>
2019-08-16 16:51:46 +0700

    Add back current_user method

--- app/controllers/snl_admin/application_controller.rb
+++ app/controllers/snl_admin/application_controller.rb
@@ -2,6 +2,13 @@ module SnlAdmin
   class ApplicationController < ActionController::Base
     protected
 
+    def current_user
+      return nil unless session[:user_id]
+
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+    helper_method :current_user
+
     def view_url(permalink, subdomain: nil, anchor: nil)
       host      = request.domain || request.host
       fullpath  = request.protocol.to_s
